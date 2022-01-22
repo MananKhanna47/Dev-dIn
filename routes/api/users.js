1803bfc083ca85e928e876e369d8161bc82cbdf3
Manan Khanna <manankhanna@Manans-MacBook-Pro.local>
2022-01-22 13:12:33 -0700

    implemented jsonwebtoken

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const {check, validationResult} =require('express-validator');
 const User = require('../../models/User');
+const config = require('config');
 router.post('/', [
     check('name', 'Name is required').not().isEmpty(), check('email', 'Please include a valid email').isEmail(),
     check('password', 'Please enter a password with 6 or more characters').isLength({min: 6})
@@ -31,7 +33,19 @@ router.post('/', [
         const salt = await bcrypt.genSalt(10);
         user.password = await bcrypt.hash(password, salt);
         await user.save();
-        res.send('User registered');
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+        jwt.sign(
+            payload, 
+            config.get('jwtSecret'),
+            {expiresIn: 360000},
+            (err, token) => {
+                if (err) throw err;
+                res.json({token});
+            });
     } catch(err){
         console.log(err.message);
         res.status(500).send('Server error');
