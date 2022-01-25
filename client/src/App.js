23ac39a292394686038764850d138558c1e0ea0b
Manan Khanna <manankhanna@Manans-MacBook-Pro.local>
2022-01-24 18:04:20 -0700

    created a redux store

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,13 @@ import { Landing } from './components/layout/Landing';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Login } from './components/auth/Login';
 import { Register } from './components/auth/Register';
+import { Provider } from 'react-redux';
+import store from './store';
+
 import './App.css';
 
 const App = () => (
+  <Provider store = {store}>
   <Router>
     <Fragment>
       <Navbar />
@@ -19,5 +23,6 @@ const App = () => (
       </section>
     </Fragment>
   </Router>
+  </Provider>
 );
 export default App;
