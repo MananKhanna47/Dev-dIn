5a290a9feee13200e1f1fc3e2a7adf4553452455
Manan Khanna <manankhanna@Manans-MacBook-Pro.local>
2022-01-26 19:47:40 -0700

    added the createProfile action

--- client/src/components/profile-forms/CreateProfile.js
+++ client/src/components/profile-forms/CreateProfile.js
@@ -1,9 +1,9 @@
 import React, { useState, Fragment } from 'react';
-import { Link, withRouter } from 'react-router-dom';
+import {  withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { createProfile } from '../../actions/profile';
 import { connect } from 'react-redux';
-
-const CreateProfile = (props) => {
+const CreateProfile = ({ createProfile, history }) => {
   const [formData, setFormData] = useState({
     company: '',
     website: '',
@@ -16,7 +16,7 @@ const CreateProfile = (props) => {
     facebook: '',
     linkedin: '',
     youtube: '',
-    instagram: '',
+    instagram: ''
   });
 
   const [displaySocialInputs, toggleSocialInputs] = useState(false);
@@ -33,11 +33,16 @@ const CreateProfile = (props) => {
     facebook,
     linkedin,
     youtube,
-    instagram,
+    instagram
   } = formData;
 
   const onChange = (e) =>
     setFormData({ ...formData, [e.target.name]: e.target.value });
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        createProfile(formData, history);
+      };
 
   return (
     <Fragment>
@@ -49,7 +54,7 @@ const CreateProfile = (props) => {
         profile stand out
       </p>
       <small>* = required field</small>
-      <form className="form">
+      <form className="form" onSubmit={(e) => onSubmit(e)}>
         <div className="form-group">
           <select name="status" value={status} onChange={(e) => onChange(e)}>
             <option value="0">* Select Professional Status</option>
@@ -149,7 +154,8 @@ const CreateProfile = (props) => {
     </Fragment>
   )
   }
-CreateProfile.propTypes = {
+  CreateProfile.propTypes = {
+  createProfile: PropTypes.func.isRequired
 };
 
-export default CreateProfile;
+export default connect(null, { createProfile })(withRouter(CreateProfile));
