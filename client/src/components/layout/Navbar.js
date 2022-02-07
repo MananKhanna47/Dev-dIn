f4e09e2cb568e2934fa24ffccb7cc0c6f23466a4
Manan Khanna <manankhanna@Manans-MacBook-Pro.local>
2022-02-07 16:35:03 -0700

    completed the profile page

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -7,6 +7,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
 
   const authLinks = (
     <ul>
+       
       <li>
         <Link to="/dashboard">
           <i className="fas fa-user" />{' '}
@@ -25,7 +26,9 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const guestLinks = (
     <ul>
       <li>
-        <Link to="/profiles">Developers</Link>
+        <Link to="/profiles">
+          Developers
+        </Link>
       </li>
       <li>
         <Link to="/register">Register</Link>
