50eefd2abd013fe5c39d7a0892395147e8088c03
Manan Khanna <manankhanna@Manans-MacBook-Pro.local>
2022-02-08 18:19:03 -0700

    bug fixes

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const connectDB = require('./config/db');
-const cors = require('cors');
 const path = require('path');
 const app = express();
 connectDB();
 app.use(express.json({extended: false}));
-app.use(cors());
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
